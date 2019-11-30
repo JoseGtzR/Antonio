2c026579c4673662b3a8fa0c14010571954e8242
Jose Gutierrez <josegtzr99@gmail.com>
2019-11-29 21:17:12 -0400

    Parcial

---
+++ Romero/TestUnitario/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void Debit1()
+        {
+            double resta = 11.99;
+            double resta1 = 0.77;
+            double esperado = 11.22;
+            BankAccount RestaTotal = new BankAccount("Jose G", resta);
+
+            RestaTotal.Debit(resta1);
+
+            double ahora = RestaTotal.Balance;
+            Assert.AreEqual(esperado, RestaTotal);
+        }
+
+
+        [TestMethod]
+        public void Credit1()
+        {
+            double suma = 11;
+            double suma1 = 0.77;
+            double esperado = 11.77;
+            BankAccount SumaTotal = new BankAccount("Jose G", suma);
+
+            SumaTotal.Debit(suma1);
+
+            double ahora = SumaTotal.Balance;
+            Assert.AreEqual(esperado, SumaTotal);
+
+        }
+    }
+}
